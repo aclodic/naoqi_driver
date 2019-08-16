dcb93bef0f656279abab9493848cd8c2a72c9ed9
Christian Dondrup <cdondrup@gmail.com>
2019-08-16 13:26:31 +0100

    Adding set language service to tts

--- naoqi_driver_services/src/naoqi_driver_service_classes/tts_services.py
+++ naoqi_driver_services/src/naoqi_driver_service_classes/tts_services.py
@@ -1,6 +1,7 @@
 from service_abstractclass import AbstractService
 from nao_interaction_msgs.srv import Say, SayResponse
 from std_srvs.srv import Empty, EmptyResponse
+from nao_interaction_msgs.srv import String, StringResponse
 
 
 class TTSServices(AbstractService):
@@ -8,8 +9,8 @@ class TTSServices(AbstractService):
         super(TTSServices, self).__init__(
             proxy_name="ALTextToSpeech",
             ns=super_ns+"/tts",
-            topics=["say", "stop_all"],
-            service_types=[Say, Empty])
+            topics=["say", "stop_all", "set_language"],
+            service_types=[Say, Empty, String])
 
     def say_callback(self, req):
         self.proxy.say(req.text)
@@ -18,3 +19,11 @@ class TTSServices(AbstractService):
     def stop_all_callback(self, _):
         self.proxy.stopAll()
         return EmptyResponse()
+
+    def set_language_callback(self, req):
+        try:
+            self.proxy.setLanguage(req.request)
+        except Exception as e:
+            return StringResponse(str(e))
+        else:
+            return StringResponse()
